d3f46ded68c5f141bcf099f8d7988171556576b1
Kera Lloyd <lloykera@gmail.com>
2025-03-22 16:40:42 -0600

    updated to add question numbers to console

--- w07.js
+++ w07.js
@@ -25,16 +25,21 @@ All questions should be printed to your Browser's console using console.log()
     c. Use a loop to iterate through the array and calculate the average age.
 */
 //Step 1a: create an array called ages
+console.log('Question 1a');
 let ages = [3, 9, 23, 64, 2, 8, 28, 93]; //create array with appropriate ages
 console.log('Ages', ages);
 let minusAge = ages[ages.length - 1] - ages[0]; //reference last number of array
 console.log('minusAge', minusAge);
 //Step 1b: Subtracting out first from last, add new age then repeat
+console.log('Question 1b');
+
 ages.push(110); //added new age
 console.log('Ages after pushing a new value', ages);
 let minusAgePush = ages[ages.length -1] - ages[0]; //dynamic
 console.log('minusAge', minusAgePush);
 //Step 1c: Calculate average age using a loop
+console.log('Question 1c');
+
 let sumOfAges = 0; 
 for (let i = 0; i < ages.length; i++) {
     //console.log(i, 'Test');
@@ -50,6 +55,8 @@ for (let i = 0; i < ages.length; i++) {
     b. Use a loop to iterate through the array again and concatenate all the names together, separated by spaces.
 */
 //Step 2a:
+console.log('Question 2a');
+
 let names = ['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob']; //Creat an array called names
 let totalChars = 0; //calculate the average number of letters per name
 
@@ -62,6 +69,8 @@ let averageName = totalChars /  names.length;// compute the average by dividing 
 console.log('Average of Names:', averageName); //Print average number of letters per name
 
 //Step 2b:
+console.log('Question 2b');
+
 let concatNames = ''; //Concat all names together with spaces
 for (let i = 0; i < names.length; i++) {
 //console.log(i, 'test');
@@ -72,10 +81,14 @@ console.log(i, 'Names concatenated', concatNames);
 /*    
 3. How do you access the last element of any array?
 */
+console.log('Question 3');
+
 console.log('Last element of ages array:', ages[ages.length - 1]);
 /*
 4. How do you access the first element of any array?
 */
+console.log('Question 4');
+
 console.log('First element of ages array:', ages[0]);
 
 /*5. Create a new array called nameLengths. Write a loop to iterate over the previously created names array and add the length of each name to the nameLengths array.
@@ -84,6 +97,8 @@ console.log('First element of ages array:', ages[0]);
     let names = ["Kelly", "Sam", "Kate"];    // starting with this array
     let nameLengths = [5, 3, 4];             // create a new array
 */
+console.log('Question 5');
+
 let nameLengths = [];
 for (let i = 0; i < names.length; i++) {
     //console.log(i, 'test');
@@ -93,6 +108,8 @@ for (let i = 0; i < names.length; i++) {
 /*
 6. Write a loop to iterate over the nameLengths array and calculate the sum of all the elements in the array.
 */
+console.log('Question 6');
+
 let charsTotal = 0;
 for(let i = 0; i< nameLengths.length; i++) {
     //console.log(i, 'test');
@@ -102,6 +119,8 @@ for(let i = 0; i< nameLengths.length; i++) {
 /*
 7. Write a function that takes two parameters, word and n, as arguments and returns the word concatenated to itself n number of times. (i.e. if I pass in 'Hello' and 3, I would expect the function to return 'HelloHelloHello').
 */
+console.log('Question 7');
+
 function concatWords(word, n) {
         console.log('Word Par:', word, 'n Par', n); // Log parameters passed into the function
         let concat = word.repeat(n); //Use the repeat() method to concat the word n times
@@ -111,6 +130,8 @@ concatWords('Hello', 3); //Call the function with 'Hello' and 3 as arguments
 /*
 8. Write a function that takes two parameters, firstName and lastName, and returns a full name. The full name should be the first and the last name separated by a space.
 */
+console.log('Question 8');
+
 function fullName(firstName, lastName) {
     let fullName = firstName + ' ' + lastName; //Concat firstName and Last Name with a space in between
     console.log(fullName); //Log the full name
@@ -119,6 +140,8 @@ fullName('Kera', 'Lloyd'); //Call the function with 'Kera' and 'Lloyd' as argume
 /*
 9. Write a function that takes an array of numbers and returns true if the sum of all the numbers in the array is greater than 100.
 */
+console.log('Question 9');
+
 //Define two example arrays
 let numbers1 = [100, 250, 300, 450];
 let numbers2 = [1, 2, 3, 4];
@@ -142,6 +165,8 @@ sumNumbersArray(numbers2); //call the function with numbers2 as the argument
 /*
 10. Write a function that takes an array of numbers and returns the average of all the elements in the array.
 */
+console.log('Question 10');
+
 function calculateNumbersAverage(array){
     let total = 0; //Initialize total to store the sum of array elements
     for (let i = 0; i < array.length; i++) { //loop through the array to sum its values
@@ -157,6 +182,8 @@ calculateNumbersAverage(numbers1); //call the function with numbers1 as the argu
 /*
 11. Write a function that takes two arrays of numbers and returns true if the average of the elements in the first array is greater than the average of the elements in the second array.
 */
+console.log('Question 11');
+
 //Define two example arrays
 let numbers3 = [50, 29, 100];
 let numbers4 = [400, 100, 50];
@@ -194,6 +221,8 @@ twoAverages(numbers3, numbers4); //Call the function with numbers3 and numbers4 
 /*
 12. Write a function called willBuyDrink that takes a boolean isHotOutside, and a number moneyInPocket, and returns true if it is hot outside and if moneyInPocket is greater than 10.50.
 */
+console.log('Question 12');
+
 function willBuyDrink(isHotOutside, moneyInPocket) {
     console.log('Parameters:', isHotOutside, moneyInPocket); //log parameters passed to the function
     let buyDrink = isHotOutside === true && moneyInPocket > 10.5;//Check if its hot outside and if the money in pocke is greater than 10.5
@@ -205,6 +234,8 @@ willBuyDrink(true, 8); //call the function with the values true and 8
 13. Create a function of your own that solves a problem. In comments, write what the function does and why you created it.
 */
 //Write a function that tells me if I have enough money to go on a cruise. We are looking to go on a cruise in the summer and I need to know if I have enough money saved up*/
+console.log('Question 13');
+
 function canGoOnCruise(savings, cruiseCost) { //Function takes two parameters- how much is saved and cost of the cruise
     if (savings >= cruiseCost) {  //Checks to see is savings is greater than or equal to the cost of the cruie
         return "You get to go on a cruise!"; //If true, it tells you you can go
